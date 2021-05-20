219ac08b880e3920f160e688d92f541ccb2910ca
Erika Leão <leaoerikaleao@pop-os.localdomain>
2021-05-20 12:26:04 -0300

    Categories

---
+++ api.js
@@ -0,0 +1,34 @@
+const categories = require('./categories.js')
+
+const main = async () => {
+   /*  await categories.create({
+      category: "Nova categoria organizada."
+   }) */
+   //await categories.update('VhjDTyt5uquypShAdL6F', {category: "Categoria Atualizada!"})
+   // await categories.remove('l97sDz3CuqzTYuVkhTAO')
+
+   const c = await categories.findAll()
+   console.log(c)
+
+   /*  const cats = await categories.findAllPaginated({ pageSize: 1, startAfter: 'twttetwtwt' })
+   console.log(cats)
+
+   await products.create({
+      product: 'Samsung A10',
+      price: 1300,
+      categories: ['IZ9AYduNCl2ijM298YXP']
+    }) */
+   //await products.remove('j2rkvbwDcPCc46rftHPn')
+
+   //await products.addImage('58tHYixXJWROawRiFW4G', { description: 'new image', url: 'url' })
+
+   /* const p = await products.findAll()
+   console.log(p)
+
+   const pPaginated = await products.findAllPaginated({ pageSize: 1, startAfter: '' })
+   console.log(pPaginated) */
+
+
+}
+
+main()
